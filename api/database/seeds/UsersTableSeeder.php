e1d297b497c9170abec328937038c83dedc02617
Miguel Huerta <miguelhuerta@Miguels-MacBook-Pro.local>
2019-12-08 10:02:02 -0800

    Base app in place. Still working on homestead. Updated the users migration to include api_token column for our API. Also create notes table

---
+++ api/database/seeds/UsersTableSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
+use Hash;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+        	'name' => 'Miguel Huerta',
+        	'email' => 'guelme88@gmail.com',
+        	'username' => 'mhuerta',
+        	'password' => Hash::make('test12'),
+        	'api_token' => Str::random(80)
+    }
+}
